e17485f70a62842a2be2655340cf05adfcb34b7b
Alexandr Savischev <alex.savishchev@gmail.com>
2018-09-25 01:20:41 +0300

    add student

--- lab1/Main.java
+++ lab1/Main.java
@@ -8,16 +8,19 @@ public class Main {
         Student student2=new Student("Дмитрий", "Линник", 3, new int[]{8, 9, 6});
         Student student3=new Student("Тимофей", "Серединский", 3, new int[]{7, 7});
         Student student4=new Student("Тимур", "Савко", 3, new int[]{9,8,7});
+        Student student5=new Student("Максим", "Бойко", 3, new int[]{9,8,7});
         System.out.println(student1+"Средний балл: "+student1.getAverageRating()+"\n");
         System.out.println(student2+"Средний балл: "+student2.getAverageRating()+"\n");
         System.out.println(student3+"Средний балл: "+student3.getAverageRating()+"\n");
         System.out.println(student4+"Средний балл: "+student4.getAverageRating()+"\n");
+        System.out.println(student5+"Средний балл: "+student5.getAverageRating()+"\n");
 
         ArrayList<Student> students = new ArrayList<>();
         students.add(student1);
         students.add(student2);
         students.add(student3);
         students.add(student4);
+        students.add(student5);
         Group group = new Group(1, students);
         System.out.println(group+"Средний балл группы: "+group.getAverageRatingOfGroup());
 
