9706faffddde537e25d8567e06a2252015db58d7
Alexandr Savischev <alex.savishchev@gmail.com>
2018-09-25 00:34:52 +0300

    deleted the student

--- lab1/Main.java
+++ lab1/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Student student2=new Student("Дмитрий", "Линник", 3, new int[]{8, 9, 6});
         Student student3=new Student("Тимофей", "Серединский", 3, new int[]{7, 7});
         Student student4=new Student("Тимур", "Савко", 3, new int[]{9,8,7});
-        Student student5=new Student("Максим","Бойко",3,new int[]{6,5,7});
+       
         System.out.println(student1+"Средний балл: "+student1.getAverageRating()+"\n");
         System.out.println(student2+"Средний балл: "+student2.getAverageRating()+"\n");
         System.out.println(student3+"Средний балл: "+student3.getAverageRating()+"\n");
