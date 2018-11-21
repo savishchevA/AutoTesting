3f0614f7642f5965c40af81a3da89f25cab9993a
Alexandr Savischev <alex.savishchev@gmail.com>
2018-11-22 01:24:01 +0300

    add web driver

---
+++ WebDriver/src/test/java/Tests.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Tests {
+
+    @Test
+    public void checkManageBooking() {
+
+        System.setProperty("webdriver.chrome.driver", ".\\src\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.qatarairways.com/en-us/homepage.html");
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(" + 0 + ","
+               + 60 + ");");
+        driver.findElement(By.id("tab2")).click();
+        driver.findElement(By.id("T7-name")).sendKeys("Savischev");
+        driver.findElement(By.id("T7-retrieve")).click();
+
+        String expectedError = "Your booking reference number should not be empty.";
+        WebElement textError = driver.findElement(By.id("checkinErrorBlock"));
+        Assert.assertEquals(textError.getText(), expectedError);
+    }
+
+}
