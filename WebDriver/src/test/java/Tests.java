7810a15f80d710ca6f96750160a0638b09753de3
Alexandr Savischev <alex.savishchev@gmail.com>
2018-11-27 13:52:25 +0300

    driver quit

--- WebDriver/src/test/java/Tests.java
+++ WebDriver/src/test/java/Tests.java
@@ -1,7 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,12 +11,10 @@ public class Tests {
     @Test
     public void checkManageBooking() {
 
-        System.setProperty("webdriver.chrome.driver", ".\\src\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
         driver.get("https://www.qatarairways.com/en-us/homepage.html");
-        ((JavascriptExecutor)driver).executeScript("window.scrollBy(" + 0 + ","
-               + 60 + ");");
         driver.findElement(By.id("tab2")).click();
         driver.findElement(By.id("T7-name")).sendKeys("Savischev");
         driver.findElement(By.id("T7-retrieve")).click();
@@ -25,6 +22,7 @@ public class Tests {
         String expectedError = "Your booking reference number should not be empty.";
         WebElement textError = driver.findElement(By.id("checkinErrorBlock"));
         Assert.assertEquals(textError.getText(), expectedError);
+        driver.quit();
     }
 
 }
